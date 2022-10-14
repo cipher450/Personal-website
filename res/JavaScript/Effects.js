cb881bc5746f8f9155e4d79099ad88fb36ee4bdb
cipher450 <hadjmus99@gmail.com>
2022-10-14 03:03:57 +0100

    update

---
+++ res/JavaScript/Effects.js
@@ -0,0 +1,29 @@
+
+//#region profile_social_icons
+$('#p_iconText1').hide()
+$('#p_iconText2').hide()
+$('#p_iconText3').hide()
+$('#p_iconText4').hide()
+
+$('.insta').hover(function(){
+    $('#p_iconText1').toggle()
+})
+$('.telegram').hover(function(){
+    $('#p_iconText2').toggle()
+
+})
+$('.github').hover(function(){
+    $('#p_iconText3').toggle()
+})
+$('.linkedin').hover(function(){
+    $('#p_iconText4').toggle()
+})
+
+//#endregion
+  
+  
+$('.Home').animate({opacity: 0}, 5);
+$(window).scroll(function(){
+    
+    $('.Home').animate({opacity: 1}, 1000);
+    })
